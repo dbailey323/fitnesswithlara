6b6b3d2758044cf3d12cfab6e077339020a5e008
dbailey323 <159084521+dbailey323@users.noreply.github.com>
2024-09-23 10:50:44 +0100

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -11,30 +11,30 @@ function displayClassDates(className, startDate, numWeeks, timeSuffix, skippedWe
     start.setHours(0, 0, 0, 0);
 
     for (let i = 0; i < numWeeks; i++) {
-        const currentDate = new Date(start);
-        currentDate.setDate(start.getDate() + (i * 7));
+    const currentDate = new Date(start);
+    currentDate.setDate(start.getDate() + (i * 7));
 
-        // Check if the week should be skipped or if the date has passed
-        if (!skippedWeeks.includes(i + 1) && currentDate >= tomorrow) {
-            const formattedDate = currentDate.toLocaleDateString("en-GB", { month: "short", day: "numeric" });
-            const linkDate = `${currentDate.getFullYear()}${(currentDate.getMonth() + 1).toString().padStart(2, '0')}${currentDate.getDate().toString().padStart(2, '0')}`;
-            const link = `https://bookwhen.com/fitnesswithlara#focus=ev-${className}-${linkDate}${timeSuffix}`;
+    // Check if the week should be skipped or if the date has passed (including today)
+    if (!skippedWeeks.includes(i + 1) && currentDate >= today) {
+        const formattedDate = currentDate.toLocaleDateString("en-GB", { month: "short", day: "numeric" });
+        const linkDate = `${currentDate.getFullYear()}${(currentDate.getMonth() + 1).toString().padStart(2, '0')}${currentDate.getDate().toString().padStart(2, '0')}`;
+        const link = `https://bookwhen.com/fitnesswithlara#focus=ev-${className}-${linkDate}${timeSuffix}`;
 
-            // Create link element
-            const linkElement = document.createElement('a');
-            linkElement.setAttribute('href', link);
-            linkElement.setAttribute('target', '_blank');
-            linkElement.innerHTML = `<small>${formattedDate}</small>`;
-            linkElement.style.backgroundColor = color;
-            linkElement.style.color = 'black';
+        // Create link element
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', link);
+        linkElement.setAttribute('target', '_blank');
+        linkElement.innerHTML = `<small>${formattedDate}</small>`;
+        linkElement.style.backgroundColor = color;
+        linkElement.style.color = 'black';
 
-            // Add a span for margin
-            const span = document.createElement('span');
-            span.style.marginRight = '10px';
+        // Add a span for margin
+        const span = document.createElement('span');
+        span.style.marginRight = '10px';
 
-            // Append elements to the container
-            classDatesContainer.appendChild(linkElement);
-            classDatesContainer.appendChild(span);
-        }
+        // Append elements to the container
+        classDatesContainer.appendChild(linkElement);
+        classDatesContainer.appendChild(span);
     }
+}
 }
